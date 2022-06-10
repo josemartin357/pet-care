49baa29a5dab361fc38ed4ba95b471cae7f1374c
Martin <martin.bedoya2021@gmail.com>
2022-06-09 19:16:59 -0700

    Added logic for login and logout routes. Front end for these routes done

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from helpers import apology, login_required
 
 
 # FLASK AND DB CONFIG HERE
+# FLASK AND DB CONFIG HERE
+# FLASK AND DB CONFIG HERE
+
 # Configure application
 app = Flask(__name__)
 
@@ -32,3 +35,39 @@ Session(app)
 
 # Configure CS50 Library to use database
 db = SQL("sqlite:///todolist.db")
+
+# ROUTES HERE
+# ROUTES HERE
+# ROUTES HERE
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    session.clear()
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        # query database for username
+        users = db.execute("SELECT * FROM users WHERE username = :username",
+                           username=request.form.get("username"))
+        # Ensure username exists and password is correct
+        if len(users) != 1 or not check_password_hash(users[0]["password"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+        # Remember which user has logged in
+        session["user_id"] = users[0]["id"]
+        # Redirect user to home page
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    session.clear()
+    return redirect("/")
