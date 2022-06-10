72d9b1dc201b4ad3e1c8ac767d95c36fa61f0bf7
Martin <martin.bedoya2021@gmail.com>
2022-06-09 18:35:54 -0700

    Added helper functions for login and display apology message for errors

--- helpers.py
+++ helpers.py
@@ -2,3 +2,35 @@
 #login function
 #apology message function
 #date and time function
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+def login_required(f):
+    """
+    Decorate routes to require login
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    # https://realpython.com/python-kwargs-and-args/
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f()
+    return decorated_function
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
