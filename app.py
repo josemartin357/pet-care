a49c1aa141e21d0cf29acc3abf992f76a00a026b
Martin <martin.bedoya2021@gmail.com>
2022-06-09 18:39:27 -0700

    Added flask configuration and db call for app.py

--- app.py
+++ app.py
@@ -1,2 +1,34 @@
 #application configurations to go here
 #routes to go here
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, send_from_directory
+from flask_session import Session
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+
+
+# FLASK AND DB CONFIG HERE
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use database
+db = SQL("sqlite:///todolist.db")
